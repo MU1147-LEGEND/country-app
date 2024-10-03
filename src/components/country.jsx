960f213fe3db60bc22df341828eb659631052f63
Mohammad Ullah <ullahmohammad314@gmail.com>
2024-10-03 21:09:24 +0600

    Refactor Country component to add local storage functionality for visited countries and improve search filter debounce

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -3,23 +3,24 @@ import { useState } from 'react';
 import CountriesInformation from './countries-information-modal';
 
 
-const Country = ({ allCountry,visitedCountries }) => {
+const Country = ({ allCountry, visitedCountries }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    
+
     const index = visitedCountries.findIndex((country) => country?.name?.common === allCountry?.name?.common);
 
-    const handleOpenModal = async() => { 
-
+    const handleOpenModal = async () => {
         // checking if index is visited or not, if country is visited then not allowed or vice versa.t
-        setIsModalOpen(true);
-
-     }
-     
+        if (visitedCountries[index].visited === false) {
+            setIsModalOpen(true);
+            visitedCountries[index].visited = true;
+            localStorage.setItem("visitedCountries", JSON.stringify(visitedCountries));
+        };
+    }
 
     let filteredCountries = { ...allCountry };
     return (
         <>
-            {filteredCountries?.region === "Americas" || filteredCountries?.region === "Europe" || filteredCountries.name.common ==="India" || filteredCountries.name.common ==="Israel" || filteredCountries.name.common ==="French Southern and Antarctic Lands" || filteredCountries.name.common === "Saint Helena, Ascension and Tristan da Cunha" ? false :
+            {filteredCountries?.region === "Americas" || filteredCountries?.region === "Europe" || filteredCountries.name.common === "India" || filteredCountries.name.common === "Israel" || filteredCountries.name.common === "French Southern and Antarctic Lands" || filteredCountries.name.common === "Saint Helena, Ascension and Tristan da Cunha" ? false :
 
                 <div className="country w-44 h-80 flex flex-col items-center justify-between bg-slate-200 dark:bg-gray-800 px-4 rounded-md py-4 ">
                     <h1 onClick={handleOpenModal} className="country-name cursor-pointer text-xl font-semibold">{filteredCountries?.name?.common}</h1>
@@ -37,7 +38,6 @@ const Country = ({ allCountry,visitedCountries }) => {
                             {visitedCountries[index].visited ? "Visited" : "More Details"}
                         </button>
                     </div>
-
 
                     <CountriesInformation
                         countryInfo={filteredCountries}
