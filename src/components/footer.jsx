7dc5fb45f487201b974f849b111117ac128b5b81
Mohammad Ullah <ullahmohammad314@gmail.com>
2024-10-03 21:58:16 +0600

    Refactor footer component to add author name in copyright notice

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <>
             <footer className='text-center mt-14 py-4 bg-slate-300 dark:bg-gray-800 w-full text-lg font-medium'>
-                <div className="year">Copyright &copy; {new Date().getFullYear()}</div>
+                <div className="year">Copyright &copy; {new Date().getFullYear()} | Mohammad Ullah</div>
             </footer>
         </>
     )
